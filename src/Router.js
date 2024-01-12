47da2f134b39e21fe2ff18422b41fca761b3e447
alejandropj <alejandroparrajimenez@gmail.com>
2024-01-12 11:35:18 +0100

    Personal y cambios en el nav para ocultar el login y cerrar sesión

--- src/Router.js
+++ src/Router.js
@@ -2,16 +2,20 @@ import {createRouter, createWebHistory} from 'vue-router';
 import HomeComponent from "./components/HomeComponent.vue"
 import LoginComponent from "./components/LoginComponent.vue"
 import RegisterComponent from "./components/RegisterComponent.vue"
+import PersonalComponent from "./components/PersonalComponent.vue"
 
 const myRoutes = [
     {
         path: '/', component : HomeComponent
     },
     {
-        path: '/login', component : LoginComponent
+        path: '/login', component : LoginComponent,  meta: { guest: true }
     },
     {
-        path: '/register', component : RegisterComponent
+        path: '/register', component : RegisterComponent,  meta: { guest: true }
+    },
+    {
+        path: '/personal', component : PersonalComponent,  meta: { requiresAuth: true }
     }
 ]
 
@@ -19,4 +23,20 @@ const router = createRouter({
     history: createWebHistory(),
     routes: myRoutes
 })
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth) {
+      const token = localStorage.getItem('token');
+      if (token) {
+        // User is authenticated, proceed to the route
+        next();
+      } else {
+        // User is not authenticated, redirect to login
+        next('/login');
+      }
+    } else {
+      // Non-protected route, allow access
+      next();
+    }
+  });
 export default router;
---
+++ src/components/PersonalComponent.vue
@@ -0,0 +1,288 @@
+<template>
+  <div>Personal</div>
+  <section style="background-color: #eee">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col">
+          <nav aria-label="breadcrumb" class="bg-light rounded-3 p-3 mb-4">
+            <div
+              class="btn-group my-3"
+              role="group"
+              aria-label="Basic radio toggle button group"
+            >
+              <input
+                type="radio"
+                class="btn-check"
+                name="btnradio"
+                id="btnradio1"
+                autocomplete="off"
+                checked
+              />
+              <label class="btn btn-sm btn-outline-dark" for="btnradio1"
+                >Datos personales</label
+              >
+
+              <input
+                type="radio"
+                class="btn-check"
+                name="btnradio"
+                id="btnradio2"
+                autocomplete="off"
+              />
+              <label class="btn btn-sm btn-outline-dark" for="btnradio2"
+                >...</label
+              >
+
+              <input
+                type="radio"
+                class="btn-check"
+                name="btnradio"
+                id="btnradio3"
+                autocomplete="off"
+              />
+              <label class="btn btn-sm btn-outline-dark" for="btnradio3"
+                >...</label
+              >
+            </div>
+          </nav>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="card mb-4">
+            <div class="card-body text-center">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
+                alt="avatar"
+                class="rounded-circle img-fluid"
+                style="width: 150px"
+              />
+              <h5 class="my-3">John Smith</h5>
+              <p class="text-muted mb-1">Full Stack Developer</p>
+              <p class="text-muted mb-4">Bay Area, San Francisco, CA</p>
+              <div class="d-flex justify-content-center mb-2">
+                <button type="button" class="btn btn-primary">Editar</button>
+                <button type="button" class="btn btn-outline-primary ms-1">
+                  Message
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="card mb-4 mb-lg-0">
+            <div class="card-body p-0">
+              <ul class="list-group list-group-flush rounded-3">
+                <li
+                  class="list-group-item d-flex justify-content-between align-items-center p-3"
+                >
+                  <i class="">🌐</i>
+                  <p class="mb-0">https://mdbootstrap.com</p>
+                </li>
+                <li
+                  class="list-group-item d-flex justify-content-between align-items-center p-3"
+                >
+                  <i class="" style="color: #333333">LinkedIn</i>
+                  <p class="mb-0">mdbootstrap</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-8">
+          <div class="card mb-4">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-sm-3">
+                  <p class="mb-0">Nombre</p>
+                </div>
+                <div class="col-sm-9">
+                  <p class="text-muted mb-0">Johnatan Smith</p>
+                </div>
+              </div>
+              <hr />
+              <div class="row">
+                <div class="col-sm-3">
+                  <p class="mb-0">Email</p>
+                </div>
+                <div class="col-sm-9">
+                  <p class="text-muted mb-0">example@example.com</p>
+                </div>
+              </div>
+              <hr />
+              <div class="row">
+                <div class="col-sm-3">
+                  <p class="mb-0">Teléfono</p>
+                </div>
+                <div class="col-sm-9">
+                  <p class="text-muted mb-0">(097) 234-5678</p>
+                </div>
+              </div>
+              <hr />
+              <div class="row">
+                <div class="col-sm-3">
+                  <p class="mb-0">Empresa</p>
+                </div>
+                <div class="col-sm-9">
+                  <p class="text-muted mb-0">Bay Area, San Francisco, CA</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="card mb-4 mb-md-0">
+                <div class="card-body">
+                  <p class="mb-4">
+                    <span class="text-primary font-italic me-1">assigment</span>
+                    Project Status
+                  </p>
+                  <p class="mb-1" style="font-size: 0.77rem">Web Design</p>
+                  <div class="progress rounded" style="height: 5px">
+                    <div
+                      class="progress-bar"
+                      role="progressbar"
+                      style="width: 80%"
+                      aria-valuenow="80"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                  <p class="mt-4 mb-1" style="font-size: 0.77rem">
+                    Website Markup
+                  </p>
+                  <div class="progress rounded" style="height: 5px">
+                    <div
+                      class="progress-bar"
+                      role="progressbar"
+                      style="width: 72%"
+                      aria-valuenow="72"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                  <p class="mt-4 mb-1" style="font-size: 0.77rem">One Page</p>
+                  <div class="progress rounded" style="height: 5px">
+                    <div
+                      class="progress-bar"
+                      role="progressbar"
+                      style="width: 89%"
+                      aria-valuenow="89"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                  <p class="mt-4 mb-1" style="font-size: 0.77rem">
+                    Mobile Template
+                  </p>
+                  <div class="progress rounded" style="height: 5px">
+                    <div
+                      class="progress-bar"
+                      role="progressbar"
+                      style="width: 55%"
+                      aria-valuenow="55"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                  <p class="mt-4 mb-1" style="font-size: 0.77rem">
+                    Backend API
+                  </p>
+                  <div class="progress rounded mb-2" style="height: 5px">
+                    <div
+                      class="progress-bar"
+                      role="progressbar"
+                      style="width: 66%"
+                      aria-valuenow="66"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="card mb-4 mb-md-0">
+                <div class="card-body">
+                  <p class="mb-4">
+                    <span class="text-primary font-italic me-1">assigment</span>
+                    Project Status
+                  </p>
+                  <p class="mb-1" style="font-size: 0.77rem">Web Design</p>
+                  <div class="progress rounded" style="height: 5px">
+                    <div
+                      class="progress-bar"
+                      role="progressbar"
+                      style="width: 80%"
+                      aria-valuenow="80"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                  <p class="mt-4 mb-1" style="font-size: 0.77rem">
+                    Website Markup
+                  </p>
+                  <div class="progress rounded" style="height: 5px">
+                    <div
+                      class="progress-bar"
+                      role="progressbar"
+                      style="width: 72%"
+                      aria-valuenow="72"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                  <p class="mt-4 mb-1" style="font-size: 0.77rem">One Page</p>
+                  <div class="progress rounded" style="height: 5px">
+                    <div
+                      class="progress-bar"
+                      role="progressbar"
+                      style="width: 89%"
+                      aria-valuenow="89"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                  <p class="mt-4 mb-1" style="font-size: 0.77rem">
+                    Mobile Template
+                  </p>
+                  <div class="progress rounded" style="height: 5px">
+                    <div
+                      class="progress-bar"
+                      role="progressbar"
+                      style="width: 55%"
+                      aria-valuenow="55"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                  <p class="mt-4 mb-1" style="font-size: 0.77rem">
+                    Backend API
+                  </p>
+                  <div class="progress rounded mb-2" style="height: 5px">
+                    <div
+                      class="progress-bar"
+                      role="progressbar"
+                      style="width: 66%"
+                      aria-valuenow="66"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "PersonalComponent",
+};
+</script>
+
+<style>
+</style>
