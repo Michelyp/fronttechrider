218f35d6c37f2ae76fe8365137a6937f60587441
alejandropj <alejandroparrajimenez@gmail.com>
2024-01-12 14:12:48 +0100

    Cambios minúsculos

--- src/components/PersonalComponent.vue
+++ src/components/PersonalComponent.vue
@@ -58,9 +58,10 @@
                 class="rounded-circle img-fluid"
                 style="width: 150px"
               />
-              <h5 class="my-3">John Smith</h5>
-              <p class="text-muted mb-1">Full Stack Developer</p>
-              <p class="text-muted mb-4">Bay Area, San Francisco, CA</p>
+              <h5 class="my-3">{{ usuario.nombre }} {{usuario.apellidos}}</h5>
+              <p class="text-muted mb-1">{{ usuario.idRole }}</p>
+
+              <p class="text-muted mb-4">{{ usuario.idEmpresaCentro }} {{usuario.idProvincia}}</p>
               <div class="d-flex justify-content-center mb-2">
                 <button type="button" class="btn btn-primary">Editar</button>
                 <button type="button" class="btn btn-outline-primary ms-1">
@@ -75,14 +76,14 @@
                 <li
                   class="list-group-item d-flex justify-content-between align-items-center p-3"
                 >
-                  <i class="">🌐</i>
-                  <p class="mb-0">https://mdbootstrap.com</p>
-                </li>
-                <li
-                  class="list-group-item d-flex justify-content-between align-items-center p-3"
-                >
-                  <i class="" style="color: #333333">LinkedIn</i>
-                  <p class="mb-0">mdbootstrap</p>
+                  <i style="color: #333333"><svg id='LinkedIn_24' width='24' height='24' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'><rect width='24' height='24' stroke='none' fill='#000000' opacity='0'/>
+
+
+<g transform="matrix(0.77 0 0 0.77 12 12)" >
+<path style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform=" translate(-13, -13)" d="M 21.125 0 L 4.875 0 C 2.183594 0 0 2.183594 0 4.875 L 0 21.125 C 0 23.816406 2.183594 26 4.875 26 L 21.125 26 C 23.816406 26 26 23.816406 26 21.125 L 26 4.875 C 26 2.183594 23.816406 0 21.125 0 Z M 8.039063 22.070313 L 4 22.070313 L 3.976563 9.976563 L 8.015625 9.976563 Z M 5.917969 8.394531 L 5.894531 8.394531 C 4.574219 8.394531 3.722656 7.484375 3.722656 6.351563 C 3.722656 5.191406 4.601563 4.3125 5.945313 4.3125 C 7.289063 4.3125 8.113281 5.191406 8.140625 6.351563 C 8.140625 7.484375 7.285156 8.394531 5.917969 8.394531 Z M 22.042969 22.070313 L 17.96875 22.070313 L 17.96875 15.5 C 17.96875 13.910156 17.546875 12.828125 16.125 12.828125 C 15.039063 12.828125 14.453125 13.558594 14.171875 14.265625 C 14.066406 14.519531 14.039063 14.867188 14.039063 15.222656 L 14.039063 22.070313 L 9.945313 22.070313 L 9.921875 9.976563 L 14.015625 9.976563 L 14.039063 11.683594 C 14.5625 10.875 15.433594 9.730469 17.519531 9.730469 C 20.105469 9.730469 22.039063 11.417969 22.039063 15.046875 L 22.039063 22.070313 Z" stroke-linecap="round" />
+</g>
+</svg></i>
+                  <a class="mb-0 link-opacity-75-hover pe-auto" :href="usuario.linkedIn">{{ usuario.linkedIn }}</a>
                 </li>
               </ul>
             </div>
@@ -96,7 +97,7 @@
                   <p class="mb-0">Nombre</p>
                 </div>
                 <div class="col-sm-9">
-                  <p class="text-muted mb-0">Johnatan Smith</p>
+                  <p class="text-muted mb-0">{{ usuario.nombre }} {{usuario.apellidos}}</p>
                 </div>
               </div>
               <hr />
@@ -105,7 +106,7 @@
                   <p class="mb-0">Email</p>
                 </div>
                 <div class="col-sm-9">
-                  <p class="text-muted mb-0">example@example.com</p>
+                  <p class="text-muted mb-0">{{ usuario.email }}</p>
                 </div>
               </div>
               <hr />
@@ -114,7 +115,7 @@
                   <p class="mb-0">Teléfono</p>
                 </div>
                 <div class="col-sm-9">
-                  <p class="text-muted mb-0">(097) 234-5678</p>
+                  <p class="text-muted mb-0">{{ usuario.telefono }}</p>
                 </div>
               </div>
               <hr />
@@ -128,7 +129,7 @@
               </div>
             </div>
           </div>
-          <div class="row">
+<!--           <div class="row">
             <div class="col-md-6">
               <div class="card mb-4 mb-md-0">
                 <div class="card-body">
@@ -271,7 +272,7 @@
                 </div>
               </div>
             </div>
-          </div>
+          </div> -->
         </div>
       </div>
     </div>
@@ -279,8 +280,23 @@
 </template>
 
 <script>
+import ServiceUsuarios from "@/services/ServiceUsuarios";
+const service = new ServiceUsuarios();
 export default {
   name: "PersonalComponent",
+  data() {
+    return {
+      usuario: {
+
+      },
+    };
+  },
+  mounted() {
+    service.GetUserByToken().then((res) => {
+      console.log(res.data);
+      this.usuario = res.data;
+    });
+  },
 };
 </script>
 
