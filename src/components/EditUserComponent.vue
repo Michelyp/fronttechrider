9552e74676b308d4b8155e2e74c0aece38b88b57
alejandropj <alejandroparrajimenez@gmail.com>
2024-01-19 14:05:51 +0100

    Test

--- src/components/EditUserComponent.vue
+++ src/components/EditUserComponent.vue
@@ -1,51 +1,52 @@
 <template>
   <div id="container-principal" class="container">
-                  <div class="container">
-                    <div class="row">
-                      <InputComponentVue
-                        class="mb-4 col-12 col-md-6"
-                        type="text"
-                        label="Nombre"
-                        id="typeNombre"
-                      />
-                      <InputComponentVue
-                        class="mb-4 col-12 col-md-6"
-                        type="number"
-                        label="Teléfono"
-                        id="typeTelefono"
-                      />
-                      <InputComponentVue
-                        class="mb-4 col-12 col-md-6"
-                        type="text"
-                        label="Provincia"
-                        id="typeProvincia"
-                      />
+    <div class="container">
+      <div class="row">
+        <h1>Hola</h1>
+        <InputComponentVue
+          class="mb-4 col-12 col-md-6"
+          type="text"
+          label="Nombre"
+          id="typeNombre"
+        />
+        <InputComponentVue
+          class="mb-4 col-12 col-md-6"
+          type="number"
+          label="Teléfono"
+          id="typeTelefono"
+        />
+        <InputComponentVue
+          class="mb-4 col-12 col-md-6"
+          type="text"
+          label="Provincia"
+          id="typeProvincia"
+        />
 
-                      <InputComponentVue
-                        class="mb-4 col-12 col-md-6"
-                        type="text"
-                        label="Apellidos"
-                        id="typeApellidos"
-                      />
+        <InputComponentVue
+          class="mb-4 col-12 col-md-6"
+          type="text"
+          label="Apellidos"
+          id="typeApellidos"
+        />
 
-                      <InputComponentVue
-                        class="mb-4 col-12 col-md-6"
-                        type="text"
-                        label="Linkedin"
-                        id="typeLinkedin"
-                      />
-
-                    </div>
-                  </div>
-                </div>
+        <InputComponentVue
+          class="mb-4 col-12 col-md-6"
+          type="text"
+          label="Linkedin"
+          id="typeLinkedin"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import InputComponentVue from "./InputComponent.vue";
 export default {
-
-}
+  name: "EditUserComponent",
+  components: InputComponentVue,
+};
 </script>
 
 <style>
-
 </style>
