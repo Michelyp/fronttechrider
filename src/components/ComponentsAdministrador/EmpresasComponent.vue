8ab04897839435b38904469bfdbeb8f00523f892
michelyp <michelypintom@gmail.com>
2024-01-25 00:33:33 +0100

    fixed: table empresas

--- src/components/ComponentsAdministrador/EmpresasComponent.vue
+++ src/components/ComponentsAdministrador/EmpresasComponent.vue
@@ -1,17 +1,17 @@
 <template>
   <TablaComponent
-    :dataTable="charlas"
+    :dataTable="empresas"
     :editable="true"
     :showBtn="true"
     :showId="true"
-    v-if="charlas.length > 0"
+    v-if="empresas.length > 0"
   />
 </template>
 
 <script>
 import TablaComponent from './../TablaComponent.vue';  
-import ServiceCharlas from '@/services/ServiceEmpresa';
-const service = new ServiceCharlas();
+import ServiceEmpresas from '@/services/ServiceEmpresa';
+const service = new ServiceEmpresas();
 
 export default {
   name: "EmpresasComponent",
@@ -29,8 +29,28 @@ export default {
             this.empresas = result.data;
             console.log(result);
         })
+    },// Para eliminar datos que necesitamos mostrar en la tabla (ID´s)
+        CleanTableView(){
+            var regex = /id|ID/;
+            this.techRiders.forEach(techRider => {               
+                Object.keys(techRider).forEach(key => {
+                    if(key.match(regex)){
+                        delete techRider[key];
+                    }
+                });
+            });
+        },
+        SelectRow(event){            
+            this.row = Array.prototype.slice.call( event.currentTarget.children );
+            this.row.forEach(cell => {
+                cell.style["background-color"] = "red";
+                console.log(cell)
+            });
+        }
+    },
+    mounted(){
+        this.loadEmpresas();
     }
-  }
 };
 </script>
 
